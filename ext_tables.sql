a0217b744806083bdbe14b39fe15524f6f37c8f7
nanna-dk <nanna@e-nanna.dk>
2022-12-13 11:40:46 +0100

    Init

---
+++ ext_tables.sql
@@ -0,0 +1,22 @@
+#
+# Add SQL definition of database tables
+#
+--
+-- Table structure for table 'tt_content'
+--
+CREATE TABLE tt_content (
+    tx_ku_semi_collapsible_accordion_item int(11) unsigned DEFAULT '0',
+);
+
+--
+-- Table structure for table 'tx_ku_semi_collapsible_accordion_item'
+--
+CREATE TABLE tx_ku_semi_collapsible_accordion_item (
+    tt_content int(11) unsigned DEFAULT '0',
+    header varchar(255) DEFAULT '' NOT NULL,
+    bodytext text,
+    media int(11) unsigned DEFAULT '0',
+    mediaorient varchar(60) DEFAULT 'left' NOT NULL,
+    imagecols tinyint(4) unsigned DEFAULT '1' NOT NULL,
+    image_zoom tinyint(3) unsigned DEFAULT '0' NOT NULL,
+);
