948c735e0aa13e44831cfff1c949f9adf9e8921a
nanna-dk <nanna@e-nanna.dk>
2023-01-02 15:48:08 +0100

    Link to open accordion

---
+++ Resources/Public/JavaScript/Src/ku-semi-collapse-accordion.js
@@ -0,0 +1,46 @@
+/**
+ * Link to open acoordion
+ */
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    /**
+     * Return if Bootstrap collapse is not present
+     */
+    if (typeof bootstrap.Collapse !== "function") {
+        return;
+    }
+
+    /**
+     * Check for accessibility settings
+     */
+    const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+    if (reduceMotion.matches) {
+        return;
+    }
+
+    /**
+     * Link to open accordion
+     */
+    const slideToOpenAccordion = () => {
+        if (window.location.hash !== '') {
+            // Open accordions based on the hash in the url
+            let accordionExists = window.location.hash.indexOf('accordion-') >= 0;
+            if (accordionExists) {
+                let accordionID = window.location.hash;
+                console.log(accordionID);
+                // Open accordion
+                const bsCollapse = new bootstrap.Collapse(accordionID, {
+                    show: true
+                });
+                // SAcroll to accordion
+                document.querySelector(accordionID).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
+    }
+
+    slideToOpenAccordion();
+});
