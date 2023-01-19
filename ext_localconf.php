3b25b5b1664b95827724637752a5248e4b5d160d
nanna-dk <nanna@e-nanna.dk>
2023-01-19 15:48:40 +0100

    Setup

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,17 +7,16 @@
  * Sep 2022 Nanna Ellegaard, University of Copenhagen.
  */
 
-defined('TYPO3_MODE') || die();
-
 defined('TYPO3') or die('Access denied.');
 
-use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+$typo3VersionNumber = VersionNumberUtility::convertVersionNumberToInteger(
+   VersionNumberUtility::getNumericTypo3Version()
+);
 // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
-if ($versionInformation->getMajorVersion() < 12) {
+if ($typo3VersionNumber < 12000000) {
     ExtensionManagementUtility::addPageTSConfig('
       @import "EXT:ku_semi_collapsible_accordion/Configuration/page.tsconfig"
    ');
