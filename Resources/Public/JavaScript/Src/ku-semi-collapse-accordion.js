91f966720578a8af29e9e489a6bbb25d08550c76
Nanna Ellegaard <nel@adm.ku.dk>
2023-04-25 10:47:01 +0200

    Updated scripts

--- Resources/Public/JavaScript/Src/ku-semi-collapse-accordion.js
+++ Resources/Public/JavaScript/Src/ku-semi-collapse-accordion.js
@@ -13,32 +13,47 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     /**
-     * Check for accessibility settings
-     */
-    const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
-    if (reduceMotion.matches) {
-        return;
-    }
-
-    /**
      * Link to open accordion
      */
     const slideToOpenAccordion = () => {
+        /**
+         * Return if Bootstrap collapse is not present
+         */
+        if (typeof bootstrap.Collapse !== 'function') {
+            return;
+        }
+
+        /**
+         * Check for accessibility settings
+         */
+        if (reduceMotion.matches) {
+            return;
+        }
         if (window.location.hash !== '') {
-            // Open accordions based on the hash in the url
-            let accordionExists = window.location.hash.indexOf('accordion-') >= 0;
+            // Open accordions or collapsible elements based on the hash in the url
+            let accordionExists = window.location.hash.indexOf('accordion-') >= 0 || window.location.hash.indexOf('collapse-') >= 0;
             if (accordionExists) {
                 const accordionID = window.location.hash; // Variable includes hash (#).
-                // Open accordion
+                // Open accordion if it exists
                 const collapsibleElement = document.querySelector(accordionID);
-                new bootstrap.Collapse(collapsibleElement).show();
-                // Scroll to accordion
-                document.querySelector(accordionID).scrollIntoView({
-                    behavior: 'smooth'
-                });
+                if (collapsibleElement) {
+                    new bootstrap.Collapse(collapsibleElement).show();
+                    // Scroll to accordion
+                    document.querySelector(accordionID).scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
             }
         }
     }
 
-    //slideToOpenAccordion();
+    slideToOpenAccordion();
+
+    window.addEventListener('orientationchange', debounce(function () {
+        slideToOpenAccordion();
+    }, 150));
+
+    window.addEventListener('resize', debounce(function () {
+        slideToOpenAccordion();
+    }, 150));
 });
